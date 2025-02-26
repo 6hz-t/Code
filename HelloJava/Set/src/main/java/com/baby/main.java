194822694bc3a0a65d4d909214187bf0d9e0376c
Tom <t-6hz@outlook.com>
2025-02-26 17:49:37 +0800

    2.26 update

--- HelloJava/Set/src/main/java/com/baby/main.java
+++ HelloJava/Set/src/main/java/com/baby/main.java
@@ -2,6 +2,7 @@ package com.baby;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.function.Consumer;
 
 public class main {
@@ -11,23 +12,23 @@ public class main {
         * 增强for，lambda，迭代器
         * */
 
-        Set<String> set = new HashSet<>();
+        Set<String> hashSet = new HashSet<>();
         //set继承于collection，方法与list类似
-        set.add("A");
-        set.add("A");//利用set的特性，用于去重
-        set.add("B");
-        set.add("C");
-        set.add("D");
-        set.add("E");
+        hashSet.add("A");
+        hashSet.add("A");//利用set的特性，用于去重
+        hashSet.add("B");
+        hashSet.add("C");
+        hashSet.add("D");
+        hashSet.add("E");
 
 
-        System.out.println(set);//[A, B, C, D, E]
+        System.out.println(hashSet);//[A, B, C, D, E]
 
 
 
 
         //1.增强for
-        for (String s : set) {
+        for (String s : hashSet) {
             System.out.print(s);
 
         }
@@ -36,7 +37,7 @@ public class main {
         //2.lambda
 
             //匿名内部类
-            set.forEach(new Consumer<String>() {
+            hashSet.forEach(new Consumer<String>() {
                 @Override
                 public void accept(String s) {
                     System.out.print(s);
@@ -47,18 +48,18 @@ public class main {
             System.out.println();
 
             //lambda表达式
-            set.forEach(s -> System.out.print(s));
+            hashSet.forEach(s -> System.out.print(s));
             System.out.println();
 
         //3.迭代器
-        set.iterator().forEachRemaining(s -> System.out.print(s));
+        hashSet.iterator().forEachRemaining(s -> System.out.print(s));
         System.out.println();
 
         System.out.println("-----------------------------set-------------------------");
 
         /*
-        HashSet 无序、无索引、无重复(hashcode、equals方法重写)
-             组成（数组、链表、红黑树）
+        HashSet 无序、无索引、无重复（数组、链表、红黑树）
+        (hashcode、equals方法重写)
 
         LinkedHashSet 有序、无索引、无重复
         对比hashset，多了一条双向链表，记录元素的存储顺序
@@ -67,13 +68,43 @@ public class main {
 
 
         /*
-        TreeSet 可排序、无索引、无重复
+        TreeSet 可排序、无索引、无重复(底层是红黑树，增删改查性能优越)
         默认Integer、String、Character排序（ASCII）
+        自定义排序：(comparable接口  负数小，正数大，0相等)
+                 1.JavaBean实现Comparable接口(默认)
 
+                 2.构造方法传入比较器Comparator接口匿名内部类——————>可简化为lambda表达式
+                    匿名内部类：
+                    TreeSet<Student> treeSet=new TreeSet<>(new Comparator<Student>() {
+                        @Override
+                        public int compare(Student o1, Student o2) {
+                            return 0;具体使用与compareTo方法一致
+                        }
+                    });
+                    此方法无需修改原代码
 
 
 
          */
+
+        Student s1=new Student("1",1);
+        Student s2=new Student("2",2);
+        Student s3=new Student("3",3);
+        Student s4=new Student("4",4);
+        Student s5=new Student("5",5);
+        Student s6=new Student("6",6);
+        Student s7=new Student("7",7);
+
+
+        TreeSet<Student> treeSet=new TreeSet<>();
+        treeSet.add(s1);
+        treeSet.add(s2);
+        treeSet.add(s3);
+        treeSet.add(s4);
+        treeSet.add(s5);
+        treeSet.add(s6);
+        treeSet.add(s7);
+        System.out.println(treeSet);
 
     }
 }
